c82a5b3dacfa6ed492e93e0bfc06ae8e7f51c44b
Andrew Barzykin <ndrewbarz@gmail.com>
2020-10-13 13:46:52 +0300

    Hashing password with bcrypt and register route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { check, validationResult } = require('express-validator');
 const router = express.Router();
-
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 // @route		POST api/users
@@ -12,13 +12,40 @@ router.post('/',[
   check('password', 'Enter a passwor with 6 or more characters').isLength({
     min: 6,
   }),
-] ,(req, res) => {
+] , async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('done')
+    const {email, password} = req.body
+
+    try {
+      // Проверяем существует ли пользователь
+      let user = await User.findOne({ email });
+      // Если существует
+      if(user) {
+        return res.status(400).json({msg: "User exist"})
+      }
+
+      // Если пользователя нет, определяем нового
+      user = new User({
+        email,
+        password
+      })
+      // перед сохранением пользователя в БД пароль нужно зашифровать (encrypted)
+      const salt = await bcrypt.genSalt(10)
+      // Хэш версия строкового пароля
+      user.password = await bcrypt.hash(password, salt)
+
+      // Сохр. юзера
+      await user.save();
+
+      res.send('User saved');
+    } catch (err) {
+      console.error(err.message)
+      res.status(500).send('Server error')
+    }
   }
 );
 
